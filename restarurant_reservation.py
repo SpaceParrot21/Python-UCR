1559c0078c8c8e6aa382b3a13370ee9602af2a7f
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-01-18 20:52:36 -0600

    Delete restarurant_reservation.py

--- restarurant_reservation.py
+++
@@ -1,8 +0,0 @@
-def table_reservation():
-    amount_of_people = int(input("How many people do you need the table for? "))
-
-    if (amount_of_people >= 8):
-        print(f"\nYou will have to wait. We don't have a table for {amount_of_people}.")
-    else:
-        print("\nYour table is ready, enjoy!")
-table_reservation()
