3f1b9622cf5b0edc6d9d1d2e8f27735447f46468
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-01-29 23:19:17 -0600

    ejercicio 5

--- Tareas/Tarea_2/ejercicio_5.py
+++ Tareas/Tarea_2/ejercicio_5.py
@@ -5,4 +5,45 @@
 # imprima en pantalla el promedio de notas del estudiante. Debe imprimirlo en
 # un diccionario de la forma {"nombre": 75} 
 
-sampleDict = {"class": {"student": {"name": "Mike","marks": {"physics": 70,"history": 80,"math: 90},}}}
+# sampleDict = { 
+#    "class":{ 
+#       "student":{ 
+#          "name":"Mike",
+#          "marks":{ 
+#             "physics":70,
+#             "history":80,
+#             "math": 90
+#          }
+#       }
+#    }
+# }
+
+# for key,val in sampleDict.items():
+#     total = val * maa
+#     print(total,key)
+
+
+sampleDict = {"class": {"student": {"name": "Mike","marks": {"physics": 70, "history": 80,"math": 90},}}}
+
+print(sampleDict["marks"])
+
+# total = 0
+
+# for i in sampleDict.values():
+#     total += i
+#     print(total)
+
+
+# sum = (70 + 80 + 90) / 3
+# print(sum)
+
+
+
+# d1 = {'a' : 15,'b' : 18,'c' : 20}
+
+# total = 0
+
+# for i in d1.values():
+#     total += i
+
+# print(total)
