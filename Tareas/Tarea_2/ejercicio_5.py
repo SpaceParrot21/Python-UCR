94f383aed5d5143f98a3f6b71b102be4e61d963e
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-01-29 21:33:56 -0600

    Nuevo file ejercicio 5

---
+++ Tareas/Tarea_2/ejercicio_5.py
@@ -0,0 +1,8 @@
+# Ejercicio 5 - Notas de clase
+
+# Descripcion:
+# Dado el siguiente diccionario, escriba un programa que
+# imprima en pantalla el promedio de notas del estudiante. Debe imprimirlo en
+# un diccionario de la forma {"nombre": 75} 
+
+sampleDict = {"class": {"student": {"name": "Mike","marks": {"physics": 70,"history": 80,"math: 90},}}}
