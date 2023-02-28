d7674e7b0ca85850b27f2c67763d4004c58955e6
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-28 17:21:58 -0600

    Agregue atributo value a __init_
    Agregue value a _init_ que va a representar el simbolo unico. 
    Agregue a __str__ el atributo self.value que va a mostrar el simbolo unico en la carta

--- Tareas/Tarea_5/Codigo Principal, Clases y Funciones/class_card.py
+++ Tareas/Tarea_5/Codigo Principal, Clases y Funciones/class_card.py
@@ -1,8 +1,9 @@
 class Card:
     
-    def __init__(self, suit, rank):
+    def __init__(self, suit, value, rank):
         self.suit = suit
         self.rank = rank
+        self.value = value
     
     def __str__(self):
-        return self.rank + ' of ' + self.suit
+        return self.rank + ' of ' + self.suit + ' ' + self.value
