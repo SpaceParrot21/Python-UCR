1736ba1f910a58c0b60747c05910473ab91683c2
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-28 17:19:11 -0600

    Agregue simbolos unicode
    Agregue simbolos unicode para la 'Hearts', 'Diamonds', 'Spades', 'Clubs'

--- Tareas/Tarea_5/Codigo Principal, Clases y Funciones/class_deck.py
+++ Tareas/Tarea_5/Codigo Principal, Clases y Funciones/class_deck.py
@@ -2,14 +2,15 @@ import class_card
 import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 class Deck:
     
     def __init__(self):
-        self.deck = []  # start with an empty list#
+        self.deck = []  # Starting with an empty list
         for suit in suits:
             for rank in ranks:
-                self.deck.append(class_card.Card(suit, rank))
+                self.deck.append(suit, suits_values[suit], rank))
     
     def __str__(self):
         deck_comp = '' #strating competition deck empty#
