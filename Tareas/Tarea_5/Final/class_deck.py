1b39d649d9f92615ded63d427885d424644c5d22
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-03-05 01:02:27 -0600

    Documentos Finales

---
+++ Tareas/Tarea_5/Final/class_deck.py
@@ -0,0 +1,35 @@
+'''Using the values suits, suit values, ranks and
+the Card Class, we generate a deck of cards, 
+shuffle our deck, and to deal out cards during gameplay'''
+
+import class_card
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+# suits = ('\u2764', '\u2666', '\u2660', '\u2618')
+suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+class Deck:
+    
+    # Methods to shuffle our deck, and to deal out cards during gameplay.
+    def __init__(self):
+        self.deck = []  # Start with an empty list
+        # instantiating all 52 unique card objects and add them to the list.
+        for suit in suits: # Iterating over sequences of suits and ranks to build out each card.
+            for rank in ranks:
+                self.deck.append(class_card.Card(suit, suits_values[suit], rank))
+    
+    def __str__(self):
+        deck_comp = '' # Starting with a competition deck empty
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__() # Add each card object's string
+        return 'The deck has' + deck_comp
+
+    # Methods to shuffle our deck,        
+    def shuffle(self):
+        random.shuffle(self.deck)
+    
+    # Method to deal out cards during gameplay
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
---
+++ Tareas/Tarea_5/Final/main_functions_module.py
@@ -0,0 +1,163 @@
+'''This module contains the functions for the Menu
+of users, show players' stats, show cards,
+results and save the results in a file'''
+
+'''All these functions require the same Global variables 
+to be able to operate correctly and show the
+desired results'''
+
+# Import Clear Terminal function to keep terminal clean
+import clear_terminal as c_terminal
+
+# Global Variables
+global USUARIOS
+global USUARIO_SELECIONADO
+
+# List of predefined users
+USUARIOS = ['Luis', 'John', 'Arturo', 'Melissa']
+# Selected Player List
+USUARIO_SELECIONADO = []
+
+# Main function of the menu
+def menu_usuarios():
+    while True:
+        print("\nUser Menu:")
+        print("1. Select an existing user")
+        print("2. Create a new user")
+        print("3. Start Game")
+        print("4. Exit")
+        opcion = input("\nSelect an Option: ")
+        c_terminal.clear()
+        if opcion == "1":
+            seleccionar_usuario()
+        elif opcion == "2":
+            crear_usuario()
+        elif opcion == "3":
+            c_terminal.clear()
+            if len(USUARIO_SELECIONADO) == 0:
+                print("\nNo user has been selected yet!!!\n")
+            else:
+                break
+        elif opcion == "4":
+            break
+        else:
+            print("Invalid Option!")
+
+# Function to display selected player statistics 
+def reading_statistics():
+
+    file_name = "player_stats.txt"
+
+    #Convert list value into 'string'
+    TEXT = "".join(USUARIO_SELECIONADO)
+    try:
+        # Opening and Reading the file
+        file_read = open(file_name, "r")
+  
+        # Reading file content line by line
+        lines = file_read.readlines()
+  
+        new_list = []
+        idx = 0
+  
+        # Looping through each line in the file
+        for line in lines:
+            # If line have the input string (TEXT), get the index
+            # of that line and put the
+            # line into newly created list
+            if TEXT in line:
+                new_list.insert(idx, line)
+                idx += 1
+  
+        # Closing file after reading
+        file_read.close()
+  
+        # If length of new list is 0 that means
+        # the input string was not
+        # found in the text file
+        if len(new_list)==0:
+            print("\n\"" +TEXT+ "\" does not have any stats in the file \"" +file_name+ "\"!")
+        else:
+            # Displaying the lines containing given string
+            lineLen = len(new_list)
+            print("\n**** These are the stats of \"" +TEXT+ "\" ****\n")
+            for i in range(lineLen):
+                print(end=new_list[i])
+            print()
+  
+    # Entering except block if input file doesn't exist
+    except:
+        print("\nThe file doesn't exist!")
+
+# Function to select an existing user
+def seleccionar_usuario():
+    print("Select an existing user")
+    if len(USUARIOS) == 0:
+        print("No registered users with this name.")
+    else:
+        print("Users:")
+        for usuario in USUARIOS:
+            print(usuario)
+        JUGADOR = input("Select an user: ")
+        c_terminal.clear()
+        if JUGADOR in USUARIOS:
+            print("\nYou have selected the user", JUGADOR,)
+            # Show player stats
+            USUARIO_SELECIONADO.append(JUGADOR)
+            reading_statistics()
+            # c_terminal.clear()
+            print( "\n---> Select option '3' to start the game or option '4' to exit <---\n")
+        else:
+            print("Usert not found")          
+
+# Function to create new user
+def crear_usuario():
+    print("Create a new user")
+    JUGADOR = input("Enter the name of the new user: ")
+    if JUGADOR in USUARIOS:
+        print("The user already exists!")
+    else:   
+        USUARIOS.append(JUGADOR)
+        USUARIO_SELECIONADO.append(JUGADOR)
+        print("User successfully created.")
+
+'''Module functions to handle game scenarios and
+save results to 'Stats' file.'''
+
+# Functions to handle game scenarios and save results to 'Estatistics' file.
+
+def player_blackjack(player,dealer):
+    print("\n ---> PLAYER HAS A BLACKJACK <---")
+    print(USUARIO_SELECIONADO, "won with a 'BLACKJACK'", player.value,".")
+    output_file = open("player_stats.txt", "a")
+    output_file.write(f"\n{USUARIO_SELECIONADO} won with a 'BLACKJACK' {player.value}.\n")
+    output_file.close()
+    
+def player_game_over(player,dealer):
+    print("\n ---> PLAYER BUSTED!!! GAME OVER!!! <---")
+    print(" ---> Dealer wins! <---")
+
+def player_wins(player,dealer):
+    print("\n ---> Player wins! <---")
+    print(USUARIO_SELECIONADO, "won with a total of", player.value,".")
+    output_file = open("player_stats.txt", "a")
+    output_file.write(f"\n{USUARIO_SELECIONADO} won with a total of {player.value}.\n")
+    output_file.close()
+
+def dealer_game_over(player,dealer):
+    print("\n ---> DEALER BUSTED!!! <---")
+    print(USUARIO_SELECIONADO, "won with a total of", player.value,".")
+    output_file = open("player_stats.txt", "a")
+    output_file.write(f"\n{USUARIO_SELECIONADO} won with a total of {player.value}.\n")
+    output_file.close()
+
+def dealer_wins(player,dealer):
+    print("\n ---> Dealer wins! GAME OVER!!!<---")
+    print("Dealer final score was: ", dealer.value,".")
+
+def push(player,dealer):
+    print("\n ---> Dealer and Player tie! It's a push. <---")
+    print(USUARIO_SELECIONADO, "tied with",player.value,".")
+    output_file = open("player_stats.txt", "a")
+    output_file.write(f"\n{USUARIO_SELECIONADO} tied with {player.value}.\n")
+    output_file.close()
